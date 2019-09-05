fc244e294207136c7b69641457c32b8923bbf97c
pauromeropau <pauromeropau@gmail.com>
2019-09-05 16:46:49 +0200

    day4

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ window.onload = function() {
   };
 
   function startGame() {
+    document.getElementById("abort-button").onclick = function() {
+      backHome();
+    };
+
     const colors = num => {
       let colorScale = d3
         .scaleLinear()
@@ -16,26 +20,33 @@ window.onload = function() {
         .range(["#f1a9a0", "#e08283"]);
       return colorScale(num);
     };
-
+    let arrayOrigin = [];
+    let arrayMoved = [];
     let selected = [];
+    let moves = 100;
+    document.getElementById("counterNum").innerHTML = `<p>${moves}</p>`;
 
+    //**************************** ARRAY ORIGIN *********************************
     let gridsArr = new Array(36).fill("").map((e, i) => {
-      return `<div class="grid" id="item-${i}" style="background-color: ${colors(
+      return `<div class="grid" id="item-${i}#" style="background-color: ${colors(
         i
       )}"></div>`;
     });
+    //****************************************************************************
 
     function shuffle(array) {
       return array.sort(() => Math.random() - 0.5);
     }
-
     document.querySelector("#grids").innerHTML = gridsArr.join(" ");
 
     setTimeout(() => {
       gridsArr = shuffle(gridsArr);
       document.querySelector("#grids").innerHTML = gridsArr.join(" ");
+    }, 1000);
+
+    setInterval(() => {
       gridsArr.forEach((grid, i) => {
-        return (document.getElementById(`item-${i}`).onclick = e => {
+        return (document.getElementById(`item-${i}#`).onclick = e => {
           if (selected.includes(e.toElement)) {
             selected = selected.filter(elem => elem !== e.toElement);
             e.toElement.classList.toggle("clickedGrid");
@@ -44,9 +55,54 @@ window.onload = function() {
             selected.push(e.toElement);
             e.toElement.classList.toggle("clickedGrid");
             e.toElement.classList.toggle("grid");
+            if (selected.length === 2) move(selected);
           }
         });
       });
-    }, 1000);
+    }, 100);
+    //**************************** ARRAY MOVED *********************************
+    function move(gridsArrSelect) {
+      let position1;
+      let position2;
+      gridsArr.forEach((elem, i) => {
+        let element = elem.substring(
+          elem.lastIndexOf("m-") + 2,
+          elem.lastIndexOf("#")
+        );
+        let elSelect1 = gridsArrSelect[0].outerHTML.substring(
+          gridsArrSelect[0].outerHTML.lastIndexOf("m-") + 2,
+          gridsArrSelect[0].outerHTML.lastIndexOf("#")
+        );
+        let elSelect2 = gridsArrSelect[1].outerHTML.substring(
+          gridsArrSelect[1].outerHTML.lastIndexOf("m-") + 2,
+          gridsArrSelect[1].outerHTML.lastIndexOf("#")
+        );
+        if (element == elSelect1) position1 = i;
+        if (element == elSelect2) position2 = i;
+      });
+      let newArray = ([gridsArr[position1], gridsArr[position2]] = [
+        gridsArr[position2],
+        gridsArr[position1]
+      ]);
+      document.querySelector("#grids").innerHTML = gridsArr.join(" ");
+      if (moves <= 1) backHome();
+      else moves--;
+      document.getElementById("counterNum").innerHTML = `<p>${moves}</p>`;
+      selected = [];
+    }
+
+    function backHome() {
+      document.getElementById("home").style.display = "grid";
+      document.getElementById("game-board").style.display = "none";
+    }
+    // arrayMoved
+    //****************************************************************************
+
+    // if(arrayOrigin === arrayMoved){
+    //   return startGame();
+    // }
   }
+  document.getElementById("reset-button").onclick = function() {
+    startGame();
+  };
 };
